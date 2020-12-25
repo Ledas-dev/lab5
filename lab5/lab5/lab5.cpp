19c7da41ff21504c2e2c0645ea4e546b7033db44
Lagumir <Legals92@mail.ru>
2020-12-25 13:33:59 +0300

    lab7

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -1,6 +1,4 @@
 //
-
-
 #include "pch.h"
 #include <iostream>
 #include <stdio.h>
@@ -16,17 +14,20 @@ void BFSD(int p,int arr);
 int **M1, *visit;
 
 void BFSD(int p, int arr){
+  int k=0;
   std::queue <int> Q;
   Q.push(p);
   visit[p] = 0;
+  printf("\n");
   while(!Q.empty()){
     p = Q.front();
-    printf("%d ", p);
     Q.pop();
-    for(int i = 0; i<=arr; i++){
-      if(M1[p][i] == 1 && visit[i] == -1){
+    k+=1;
+    printf("v[%d]=%d ",k, p);
+    for(int i = 0; i<arr; i++){
+      if(M1[p][i] > 0 && visit[i] > visit[p] + M1[p][i]){
         Q.push(i);
-        visit[i] = visit[p] + 1;
+        visit[i] = visit[p] + M1[p][i];
       }
     }
   }
@@ -50,51 +51,42 @@ float k;
   visit = (int*)malloc(arr*sizeof(int));
 
   for(int i = 0;i<arr;i++){
-    visit[i] = -1;
+    visit[i] = 1000;
   }
 
   printf("Введем матрицу:\n");
   for(int i = 0;i<arr;i++){
     for(int j = 0;j<i+1;j++){
-       k = rand()%100;
-       if(k>70){
-         k = 1;
-       }else{
+       k = rand()%10;
+       if(i == j){
          k = 0;
        }
-      if(i != j ){
+       if(k>5){
+         k = 0;
+       }
         M1[i][j] = k;
         M1[j][i] = k;
-      }else{
-        M1[i][j] = 0;
-      }
+
     }
   }
   for(int i = 0;i<arr;i++){
     for(int j = 0;j<arr;j++){
-      printf("%d ",M1[i][j] );
+      printf("%d  ",M1[i][j] );
     }
     printf("\n");
   }
   printf("\n");
-  for(int i = 0;i<1;i++){
-  printf("Выберете вершину с которойъ начанете обход:");
-  scanf("%d", &p);
-    if(p<=arr){
-      printf("\nВы проходите");
+  for(int i = 0;i<arr;i++){
 
-    }else{
-      printf("Выберете новую координату\n");
-      i--;
-    }
-  }
+    p = i;
 
   printf("\n");
     BFSD(p, arr);
     printf("\n");
 
     for(int i=0; i<arr; i++){
-      printf("%d ", visit[i]);
+      printf("\n%d", visit[i]);
+      visit[i]=1000;
     }
-
+  }
 }
